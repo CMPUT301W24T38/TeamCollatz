aa558f471ae79fcf9ba835ec8da4a00dd8919082
Nathan Thai <n3cr0d3m0nncrdmn@gmail.com>
2024-02-01 16:03:25 -0700

    resolved conflict

--- app/src/main/java/com/example/teamcollatz/Shape.java
+++ app/src/main/java/com/example/teamcollatz/Shape.java
@@ -3,9 +3,5 @@ package com.example.teamcollatz;
 public abstract class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
     String color = "blue";
-=======
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
->>>>>>> 3fe1295908c8643f1e35345844e6c6cf370f2033
 }
